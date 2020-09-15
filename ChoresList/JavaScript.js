0a4dab4ef23366a847986e7ce8e40f8e1eaeb613
Alexandros Nicolaou <alnicolaou96@gmail.com>
2020-09-15 10:52:02 -0400

    close fh dropdown done

--- ChoresList/JavaScript.js
+++ ChoresList/JavaScript.js
@@ -1,3 +1,18 @@
-﻿function fhDropdown() {
-    document.getElementById("fhDropdown").style.display = "block";
+﻿
+//field house chores display
+function fhDropdown() {
+    document.getElementById("fhDropdown").style.display = "block";//opens dropdown
+}
+
+function fhFirstChores() {
+    document.getElementById("fhDropdown").style.display = "none";//closes dropdown
+}
+function fhSecondChores() {
+    document.getElementById("fhDropdown").style.display = "none";//closes dropdown
+}
+function fhThirdChores() {
+    document.getElementById("fhDropdown").style.display = "none";//closes dropdown
+}
+function fhFourthChores() {
+    document.getElementById("fhDropdown").style.display = "none";//closes dropdown
 }
